4b498c15bb62a7a733d9a242144dd6ca91426a96
Maple1302 <95062292+Maple1302@users.noreply.github.com>
2024-10-21 11:46:18 +0700

    update signupview

--- lib/src/views/signupview.dart
+++ lib/src/views/signupview.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_any_logo/flutter_logo.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+
 
 class SignUpForm extends StatefulWidget {
   const SignUpForm({super.key});
@@ -17,7 +22,11 @@ class _SignUpFormState extends State<SignUpForm> {
   bool _isEmailValid = false;
   bool _isPasswordValid = false;
   bool _isRetypePasswordValid = false;
-
+  final TextStyle baseTextStyle = GoogleFonts.baloo2(fontSize: 18);
+  final TextStyle hintTextStyle = GoogleFonts.baloo2(fontSize: 15, color: Colors.blueGrey);
+  final TextStyle buttonTextStyle = GoogleFonts.baloo2(fontSize: 18, color: Colors.white);
+  final Color buttonColor = Color.fromRGBO(70,140,255,1);
+  final  double sizeIcon = 100.0;
   void _validateInputs() {
     setState(() {
       _isNameValid = _nameController.text.isNotEmpty;
@@ -56,35 +65,45 @@ class _SignUpFormState extends State<SignUpForm> {
         ListView(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
           children: [
-            Padding(
-              padding:
-                  const EdgeInsets.only(top: 80.0), // Thay thế SizedBox bằng Padding
+
+    Padding(
+      padding: const EdgeInsets.only(top: 50.0),
+      child: Center(
+        child: SizedBox(
+        height: 150,
+        width: 150, // Đặt chiều rộng bằng chiều cao để giữ hình tròn
+        child: ClipOval(
+        child:  Image.asset(
+        'assets/images/Logo_app.png', // Thay đổi link logo tại đây
+        fit: BoxFit.cover, // Giữ tỷ lệ hình ảnh
+        ),
+
+        ),
+        ),
+      ),
+    ),
+
+
+
+
+
+            Center(
               child: Text(
-                'Create New Account',
-                style:GoogleFonts.baloo2(fontSize: 15,)
-              ),
+                  'Create New Account',
+                  style:baseTextStyle.copyWith(fontSize: 25,fontWeight: FontWeight.bold)
+                ),
             ),
-            const Padding(
-              padding: EdgeInsets.only(top: 10.0,bottom: 50), // Thêm khoảng cách bên trên
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Sign in to watch more movie',
-                    style: TextStyle(color: Colors.grey, fontSize: 18),
-                  ),
-                ],
-              ),
-            ),
-      
+
+
+
             textFieldItem(
-                "Name", _nameController, _validateInputs, _isNameValid),
+                "Name","Enter your name", _nameController, _validateInputs, _isNameValid),
             textFieldItem(
-                "Email", _emailController, _validateInputs, _isEmailValid),
-            textFieldItem("Password", _passwordController, _validateInputs,
+                "Email","Enter your email", _emailController, _validateInputs, _isEmailValid),
+            textFieldItem("Password","Enter your password", _passwordController, _validateInputs,
                 _isPasswordValid,
                 isPasswordFeild: true),
-            textFieldItem("Retype Password", _passwordRetypeController,
+            textFieldItem("Retype Password","Enter your retype password", _passwordRetypeController,
                 _validateRetypePassword, _isRetypePasswordValid,
                 isPasswordFeild: true),
             Padding(
@@ -92,56 +111,60 @@ class _SignUpFormState extends State<SignUpForm> {
                   top: 30.0), // Khoảng cách trước nút Sign-Up
               child: Center(
                   child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(72,122,255, 1),
-                    borderRadius: BorderRadius.circular(30)),
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.all(10),
-                      backgroundColor: Colors.green,
+                    width: double.infinity, // Đặt chiều rộng đầy đủ
+
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+
+                        backgroundColor: buttonColor, // Đặt màu nền cho button
+
+                      //  disabledBackgroundColor: buttonColor,
+
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18), // Đặt bán kính bo tròn
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 15), // Đặt padding cho button
+                        elevation: 0, // Đặt độ cao của button là 0 để không có viền bóng
+                      ),
+                      onPressed:  () {
+                        // Thực hiện logic đăng ký ở đây
+                      }
+                          ,
+                      child: Text(
+                        "Register",
+                        style: buttonTextStyle,
+                      ),
                     ),
-                    onPressed: _isNameValid && _isEmailValid && _isPasswordValid
-                        ? () {
-                            // Perform sign-up logic here
-                          }
-                        : null,
-                    child: Text(
-                      "Register",
-                      style: TextStyle(color: Colors.white),
-                    )),
-              )),
+                  )
+                ,
+              ),
             ),
-            const Padding(
-              padding: EdgeInsets.only(top: 20.0), // Khoảng cách cho chữ "or"
+             Padding(
+              padding: EdgeInsets.only(top: 10.0), // Khoảng cách cho chữ "or"
               child: Center(
-                child: Text('or', style: TextStyle(color: Colors.grey)),
+                child: Text('or', style: baseTextStyle.copyWith(color: Colors.grey)),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(
-                  top: 10.0), // Khoảng cách cho icon mạng xã hội
+              padding: const EdgeInsets.only(top:10,
+                  bottom: 50), // Khoảng cách cho icon mạng xã hội
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  IconButton(
-                    icon: const Icon(Icons.g_translate, color: Colors.red),
-                    onPressed: () {
-                      // Google sign-up logic
-                    },
+                  SvgPicture.asset(
+                    'assets/images/logos/google.svg', // Replace with actual SVG URL
+                    height: 50,
+                    width: 50,
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.facebook, color: Colors.blue),
-                    onPressed: () {
-                      // Facebook sign-up logic
-                    },
+                  SvgPicture.asset(
+                    'assets/images/logos/facebook.svg', // Replace with actual SVG URL
+                    height: 50,
+                    width: 50,
                   ),
-                  IconButton(
-                    icon:
-                        const Icon(Icons.flutter_dash, color: Colors.lightBlue),
-                    onPressed: () {
-                      // Twitter sign-up logic
-                    },
+                  SvgPicture.asset(
+                    'assets/images/logos/gmail.svg', // Replace with actual SVG URL
+                    height: 50,
+                    width: 50,
                   ),
                 ],
               ),
@@ -153,7 +176,7 @@ class _SignUpFormState extends State<SignUpForm> {
   }
 }
 
-Widget textFieldItem(String label, TextEditingController controller,
+Widget textFieldItem(String label,String hintText, TextEditingController controller,
     Function onChanged, bool isValid,
     {bool isPasswordFeild = false}) {
   return Column(
@@ -162,22 +185,22 @@ Widget textFieldItem(String label, TextEditingController controller,
         alignment: Alignment.topLeft,
         child: Text(
           label,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          style: GoogleFonts.baloo2(fontSize: 18)
         ),
       ),
       Padding(
         padding: const EdgeInsets.only(bottom: 20,left: 4,right: 4,top:4),
         child: TextField(
           decoration: InputDecoration(
-            hintText: 'Enter your name',
-            hintStyle: TextStyle(fontSize: 14,color: Colors.blueGrey),
+            hintText: hintText,
+            hintStyle: TextStyle(fontSize: 15,color: Colors.blueGrey),
             border: OutlineInputBorder(
               borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(30.0),
+              borderRadius: BorderRadius.circular(15.0),
             ),
             filled: true,
             fillColor: Colors
-                .white, 
+                .white,
           ),
         ),
       )
